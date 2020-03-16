51e4e9c77f0fffbaf4ee01bcbb65d43bbb03c32f
Ayham Samara <Ayham.Samara@live.com>
2020-03-16 15:43:18 +0200

    React task 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-
-import authors from "./data.js";
+import axios from "axios";
+import Loading from "./Loading.js";
 
 // Components
 import Sidebar from "./Sidebar";
@@ -9,20 +9,65 @@ import AuthorDetail from "./AuthorDetail";
 
 class App extends Component {
   state = {
-    currentAuthor: null
+    currentAuthor: null,
+    authors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  // selectAuthor = author => this.setState({ currentAuthor: author });
+  selectAuthor = async id => {
+    try {
+      this.setState({
+        loading: true
+      });
+      const response = await axios.get(
+        `https://the-index-api.herokuapp.com/api/authors/${id}/`
+      );
+      let author = response.data;
+      this.setState({
+        currentAuthor: author,
+        loading: false
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   getContentView = () => {
-    if (this.state.currentAuthor) {
+    if (this.state.loading) {
+      return <Loading />;
+    } else if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
-      return <AuthorList authors={authors} selectAuthor={this.selectAuthor} />;
+      return (
+        <AuthorList
+          authors={this.state.authors}
+          selectAuthor={this.selectAuthor}
+        />
+      );
     }
   };
+
+  getAuthors = async () => {
+    try {
+      const response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+      );
+      const authors = response.data;
+      this.setState({
+        authors: authors,
+        loading: false
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  componentDidMount() {
+    this.getAuthors();
+  }
 
   render() {
     return (
